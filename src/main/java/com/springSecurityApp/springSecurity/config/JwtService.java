5e8091a7d433a5f17f480a77cbe37156c97c0e02
Zied <zied.kannou15@gmail.com>
2024-03-10 13:13:54 +0100

    username extraction from token

--- src/main/java/com/springSecurityApp/springSecurity/config/JwtService.java
+++ src/main/java/com/springSecurityApp/springSecurity/config/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -14,7 +15,13 @@ public class JwtService {
     private static final String SECRET_KEY = "FwcIfbLY32CSL8PoNdhDCFYUpLxRSlHcoH3S21lpXK2m0C5v/Xyr/G3Q1uTMmiVYs3fH2+LQJftuOnCYNJ8lO1/Q/2YFVg35tzcWiB9RABY9eIEbrpMmtAchE87Bm/NAW1iwz76pKkg7PGEW1nPD2VEZpVJXLHYu0/pUFYSz7okKBp1bhUZlBpcNoUGVBWEar1h7T77lf3jF3T1HNf/I+XbITXYRFVTbJVXfg1vPzEZ1uMoGAKdYAyerSHMNULvid835+e+pTwh5fC7BA19hxNft8xhEFZKZzTRFmSiGL12YncBHrX4Q0BtyHWAtLWasj9arY3WXt8wJnNkTNq6vK3A8qbu3olH+HnVw4Pbh/74=\n"
 
     public String extractUsername(String token) {
-        return null;
+        //the subject of the token which is the email/username
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim (String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     public Claims extractAllClaims(String token) {
