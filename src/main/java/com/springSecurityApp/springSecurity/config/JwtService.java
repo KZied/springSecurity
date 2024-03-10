19a06b7b0c1f7e2c4e7a98894155cf79d5b08485
Zied <zied.kannou15@gmail.com>
2024-03-10 13:58:18 +0100

    token validation

--- src/main/java/com/springSecurityApp/springSecurity/config/JwtService.java
+++ src/main/java/com/springSecurityApp/springSecurity/config/JwtService.java
@@ -47,6 +47,19 @@ public class JwtService {
                 .compact();
 
     }
+    
+    public boolean isTokenValid (String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+    
+    private boolean isTokenExpired (String token) {
+        return  extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 
     public Claims extractAllClaims(String token) {
         return Jwts
